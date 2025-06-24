dff0b25b9250497edb0384bddd418c0edec753e9
Tomaslevys <141672471+Tomaslevys@users.noreply.github.com>
2025-06-24 14:21:40 -0300

    avances con cloudinary

---
+++ cloudinary/middleware/multer.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const uploadDir = join(__dirname, "../uploads");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Tipo de archivo inválido.'), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+export default upload;
